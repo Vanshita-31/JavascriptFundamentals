57864b9296703aaed99b4f5f1dcbde7fdf8f04d1
Soham Halbandge <57310431+Logzii@users.noreply.github.com>
2023-05-03 15:58:29 +0530

    session 1

---
+++ Session1.js
@@ -0,0 +1,68 @@
+// console.log("Hello,World!");
+// console.log("Soham");
+// console.log(2);
+// console.log('This is a string');
+// console.log("This is also a string");
+// console.log('');
+// console.log('a');
+// console.log(true);
+// console.log(false);
+// console.log(undefined);
+// console.log(null);
+
+
+// checking the type of data types
+// console.log(typeof (2));
+// console.log(typeof (undefined));
+// console.log(typeof ('str'));
+// console.log(typeof (true));
+// console.log(typeof (false));
+
+// console.log(typeof (typeof (1)));
+// console.log(typeof (1));
+
+// var a = 'this is a long paragraph';
+
+// var a; // Declaration part
+// a = 'this is a long paragraph'; // assigning value to the declared variable
+// console.log(a);
+
+// a = 'this is a even longer paragraph'; // Re-assignment
+// /* 
+// Var is not required again for reassigning the value
+// */
+// var a = 10; // Re-declaration and assignment
+// console.log(a);
+
+// var greeter = "hey hi";
+// console.log(greeter);
+// var greeter = "say Hello instead";
+// console.log(greeter);
+
+// const a = 5; 
+// // const a = 6; Re-declaration and re-assignment not allowed
+// console.log(a); 
+
+// let b = 10;
+// // let a = 11;
+// console.log(b);
+// b = 11;
+// console.log(b);
+
+// var $variable = 'ghost'; // valid name
+// console.log($variable);
+
+// var x = 0;
+// console.log(typeof(typeof(x)));
+
+var a;
+console.log(a);
+
+var result = 'null';
+console.log(a);
+
+result = 93;
+console.log(result);
+
+// undefined --> unintentionally missing values
+// defined --> intentionally missing values
